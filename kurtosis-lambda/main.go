35e02414328f054647e5249e7be535caca482fdb
Leandro Poroli <leandro.poroli@kurtosistech.com>
2021-09-09 09:56:12 -0300

    Initial commit

---
+++ kurtosis-lambda/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kurtosis-tech/kurtosis-lambda-api-lib/golang/lib/execution"
+	"github.com/kurtosis-tech/ethereum-kurtosis-lambda/kurtosis-lambda/impl"
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+const (
+	successExitCode = 0
+	failureExitCode = 1
+)
+
+func main() {
+
+	// >>>>>>>>>>>>>>>>>>> REPLACE WITH YOUR OWN CONFIGURATOR <<<<<<<<<<<<<<<<<<<<<<<<
+	configurator := impl.NewExampleKurtosisLambdaConfigurator()
+	// >>>>>>>>>>>>>>>>>>> REPLACE WITH YOUR OWN CONFIGURATOR <<<<<<<<<<<<<<<<<<<<<<<<
+
+
+	lambdaExecutor := execution.NewKurtosisLambdaExecutor(configurator)
+	if err := lambdaExecutor.Run(); err != nil {
+		logrus.Errorf("An error occurred running the Kurtosis Lambda executor:")
+		fmt.Fprintln(logrus.StandardLogger().Out, err)
+		os.Exit(failureExitCode)
+	}
+	os.Exit(successExitCode)
+}
