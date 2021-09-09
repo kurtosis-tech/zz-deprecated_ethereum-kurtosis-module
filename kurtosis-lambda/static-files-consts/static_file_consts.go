0d85f8b0f20160f7feb76a71526dcec3c42b44e9
Leandro Poroli <leandro.poroli@kurtosistech.com>
2021-09-09 10:59:30 -0300

    Add static files and static files constants

---
+++ kurtosis-lambda/static-files-consts/static_file_consts.go
@@ -0,0 +1,25 @@
+package static_files_consts
+
+import (
+	"github.com/kurtosis-tech/kurtosis-client/golang/lib/services"
+	"path"
+)
+
+const (
+	// Directory where static files live inside the testsuite container
+	staticFilesDirpathOnTestsuiteContainer = "/static-files"
+
+	GenesisStaticFileID    services.StaticFileID = "genesis"
+	PasswordStaticFileID   services.StaticFileID = "password"
+	SignerKeystoreFileID   services.StaticFileID = "signer-key"
+
+	genesisStaticFileName                        = "genesis.json"
+	passwordStaticFileName                       = "password.txt"
+	signerKeystoreFileName                       = "UTC--2021-08-11T21-30-29.861585000Z--14f6136b48b74b147926c9f24323d16c1e54a026"
+)
+
+var StaticFileFilepaths = map[services.StaticFileID]string{
+	GenesisStaticFileID: path.Join(staticFilesDirpathOnTestsuiteContainer, genesisStaticFileName),
+	PasswordStaticFileID: path.Join(staticFilesDirpathOnTestsuiteContainer, passwordStaticFileName),
+	SignerKeystoreFileID: path.Join(staticFilesDirpathOnTestsuiteContainer, signerKeystoreFileName),
+}
