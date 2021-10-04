fe73f0ac1ff4a61bd362ade8ba4ccb7266999e50
Leandro Poroli <leandro.poroli@kurtosistech.com>
2021-10-04 11:52:49 -0300

    Upgrade Kurt Lambda Lib to v0.9.1 and adapt Eth Lambda

--- kurtosis-lambda/static-files-consts/static_file_consts.go
+++ kurtosis-lambda/static-files-consts/static_file_consts.go
@@ -1,31 +1,12 @@
 package static_files_consts
 
-import (
-	"github.com/kurtosis-tech/kurtosis-client/golang/lib/services"
-	"path"
-)
-
 const (
 	// Directory where static files live inside the testsuite container
-	staticFilesDirpathOnTestsuiteContainer = "/static-files"
+	StaticFilesDirpathOnTestsuiteContainer = "/static-files"
 
-	GenesisStaticFileID               services.StaticFileID = "genesis"
-	SignerAccountPasswordStaticFileID services.StaticFileID = "password"
-	SignerKeystoreFileID              services.StaticFileID = "signer-key"
-
-	genesisStaticFileName               = "genesis.json"
-	signerAccountPasswordStaticFileName = "password.txt"
-	signerKeystoreFileName              = "UTC--2021-08-11T21-30-29.861585000Z--14f6136b48b74b147926c9f24323d16c1e54a026"
+	GenesisStaticFileName               = "genesis.json"
+	SignerAccountPasswordStaticFileName = "password.txt"
+	SignerKeystoreFileName              = "UTC--2021-08-11T21-30-29.861585000Z--14f6136b48b74b147926c9f24323d16c1e54a026"
 )
 
-var StaticFiles = map[services.StaticFileID]bool{
-	GenesisStaticFileID:               true,
-	SignerAccountPasswordStaticFileID: true,
-	SignerKeystoreFileID:              true,
-}
-
-var StaticFileFilepaths = map[services.StaticFileID]string{
-	GenesisStaticFileID:               path.Join(staticFilesDirpathOnTestsuiteContainer, genesisStaticFileName),
-	SignerAccountPasswordStaticFileID: path.Join(staticFilesDirpathOnTestsuiteContainer, signerAccountPasswordStaticFileName),
-	SignerKeystoreFileID:              path.Join(staticFilesDirpathOnTestsuiteContainer, signerKeystoreFileName),
-}
+var StaticFilesNames = []string{GenesisStaticFileName, SignerAccountPasswordStaticFileName, SignerKeystoreFileName}
